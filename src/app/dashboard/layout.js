a9f92bf150cd01badf95459be99d73fcff0e0a26
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-06-29 22:30:57 +0300

    feat: add grid system for dashboard layout

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import styles from "./layout.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.gridContainer}>
+      <div className={styles.header}>Header</div>
+      <div className={styles.navbar}>Navbar</div>
+      <div className={styles.main}>{children}</div>
+    </div>
+  );
+}
