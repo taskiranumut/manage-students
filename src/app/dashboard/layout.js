5da218442c82c680c5468878480cd56e73060081
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-06-29 23:33:28 +0300

    feat: add Header to dashboard layout

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,11 +2,14 @@
 
 import React from "react";
 import styles from "./layout.module.css";
+import Header from "@/components/Header";
 
 export default function Layout({ children }) {
   return (
     <div className={styles.gridContainer}>
-      <div className={styles.header}>Header</div>
+      <div className={styles.header}>
+        <Header />
+      </div>
       <div className={styles.navbar}>Navbar</div>
       <div className={styles.main}>{children}</div>
     </div>
