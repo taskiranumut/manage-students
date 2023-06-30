323da370c374a18147cc44f9665ad46a217d8c47
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-06-30 22:58:53 +0300

    feat: create general SearchBar component

---
+++ src/components/Forms/SearchBar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Form from "@/components/Forms/Form";
+import Search from "@/components/Icons/Search";
+
+export default function SearchBar() {
+  return (
+    <Form>
+      <div className={styles.inputWrapper}>
+        <input type="text" className={styles.input} placeholder="Search..." />
+        <button className={styles.iconBtn}>
+          <Search />
+        </button>
+      </div>
+    </Form>
+  );
+}
