9d787ad447bd88b6ded51deff8e65a3748296737
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-06-30 15:42:30 +0300

    feat: add AppTitle and Profile into Sidebar

--- src/features/Dashboard/components/Sidebar/index.js
+++ src/features/Dashboard/components/Sidebar/index.js
@@ -1,6 +1,19 @@
 import React from "react";
 import styles from "./styles.module.css";
+import AppTitle from "@/components/AppTitle";
+import Profile from "./Profile";
 
 export default function Sidebar() {
-  return <aside className={styles.sidebar}></aside>;
+  return (
+    <aside className={styles.sidebar}>
+      <section>
+        <AppTitle size="medium" />
+        <Profile
+          fullName="John Doe"
+          userType="Admin"
+          imgUrl="/images/avatar.png"
+        />
+      </section>
+    </aside>
+  );
 }
