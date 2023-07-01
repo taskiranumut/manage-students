46c8818d37d86a28905fede6bf02f0cc60d6bccc
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-01 19:04:03 +0300

    feat: redirect to dashboard route after sign-in

--- src/features/Authentication/SignIn/components/SignInForm/index.js
+++ src/features/Authentication/SignIn/components/SignInForm/index.js
@@ -1,15 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormItems/FormInput";
 import Button from "@/components/Forms/Button";
+import { useRouter } from "next/navigation";
 
 export default function SignInForm() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    setForm((form) => ({
+      ...form,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!form.email || !form.password) return;
+
+    router.replace("/dashboard");
+  };
+
   return (
-    <Form>
-      <FormInput type="text" placeholder="Enter your email" label="Email" />
+    <Form onSubmit={handleSubmit}>
+      <FormInput
+        type="text"
+        name="email"
+        onChange={handleChange}
+        value={form.email}
+        placeholder="Enter your email"
+        label="Email"
+      />
       <FormInput
         type="password"
+        name="password"
+        onChange={handleChange}
+        value={form.password}
         placeholder="Enter your password"
         label="Password"
       />
