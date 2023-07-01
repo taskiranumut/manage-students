781d2ea9ae18643083657ee45a60f810d4ae0ccd
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-02 00:32:40 +0300

    feat: fetch students data and list in students route

--- src/features/Dashboard/Students/components/Main/index.js
+++ src/features/Dashboard/Students/components/Main/index.js
@@ -1,57 +1,13 @@
+"use client";
+
 import React from "react";
 import styles from "./styles.module.css";
 import StudentTable from "@/features/Dashboard/Students/components/StudentTable";
 
-export default function Main() {
+export default function Main({ bodyData }) {
   const tableData = {
     headerData: ["", "name", "email", "phone", "website", "company name"],
-    bodyData: [
-      {
-        id: "1",
-        imgUrl: "/images/avatar.png",
-        studentName: "John Doe 1",
-        email: "john@gmail.com",
-        phone: "7305477760",
-        website: "karthi.lorem.com",
-        companyName: "karthi.lorem.com",
-      },
-      {
-        id: "2",
-        imgUrl: "/images/avatar.png",
-        studentName: "John Doe 2",
-        email: "john@gmail.com",
-        phone: "7305477760",
-        website: "karthi.lorem.com",
-        companyName: "karthi.lorem.com",
-      },
-      {
-        id: "3",
-        imgUrl: "/images/avatar.png",
-        studentName: "John Doe 3",
-        email: "john@gmail.com",
-        phone: "7305477760",
-        website: "karthi.lorem.com",
-        companyName: "karthi.lorem.com",
-      },
-      {
-        id: "4",
-        imgUrl: "/images/avatar.png",
-        studentName: "John Doe 4",
-        email: "john@gmail.com",
-        phone: "7305477760",
-        website: "karthi.lorem.com",
-        companyName: "karthi.lorem.com",
-      },
-      {
-        id: "5",
-        imgUrl: "/images/avatar.png",
-        studentName: "John Doe 5",
-        email: "john@gmail.com",
-        phone: "7305477760",
-        website: "karthi.lorem.com",
-        companyName: "karthi.lorem.com",
-      },
-    ],
+    bodyData: bodyData,
   };
 
   return <StudentTable tableData={tableData} />;
--- src/features/Dashboard/Students/index.js
+++ src/features/Dashboard/Students/index.js
@@ -1,17 +1,35 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState, useMemo } from "react";
+import useFetch from "@/hooks/useFetch";
+import { rebaseData } from "@/features/Dashboard/Students/utils";
 import styles from "./styles.module.css";
 import Header from "@/features/Dashboard/Students/components/Header";
 import Main from "@/features/Dashboard/Students/components/Main";
 import Footer from "@/features/Dashboard/Students/components/Footer";
 
 export default function Students() {
+  const [studentList, setStudentList] = useState([]);
+
+  const url =
+    "https://dummyjson.com/users?limit=5&select=firstName,lastName,email,phone,image,company,website";
+  const options = useMemo(() => ({ method: "GET" }), []);
+  const { data, loading, error } = useFetch(url, options);
+
+  useEffect(() => {
+    if (!data) return;
+
+    const rebasedData = rebaseData(data?.users);
+    setStudentList(rebasedData);
+  }, [data]);
+
   return (
     <>
       <section className={styles.header}>
         <Header title="Student List" />
       </section>
       <section className={styles.content}>
-        <Main />
+        <Main bodyData={studentList} />
       </section>
       <section className={styles.footer}>
         <Footer />
