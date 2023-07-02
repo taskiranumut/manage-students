299d72f41dc348955b9163413b38562835ebf89c
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-02 16:16:35 +0300

    feat: create edit-student dynamic route

---
+++ src/app/dashboard/edit-student/[id]/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+import styles from "./page.module.css";
+import StudentDetail from "@/features/Dashboard/Students/components/StudentDetail";
+
+export default function Page() {
+  return (
+    <main className={styles.main}>
+      <StudentDetail isAdd={false} />
+    </main>
+  );
+}
