9c310f950bdefeada1a863594c4d3db6538eecb5
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-02 13:12:57 +0300

    feat: use student list context in Students

--- src/features/Dashboard/Students/index.js
+++ src/features/Dashboard/Students/index.js
@@ -3,6 +3,7 @@
 import React, { useEffect, useState, useMemo } from "react";
 import styles from "./styles.module.css";
 import useFetch from "@/hooks/useFetch";
+import { useStudentListContext } from "@/contexts/StudentList/context";
 import { rebaseData } from "@/features/Dashboard/Students/utils";
 import { baseUrl } from "@/features/Dashboard/Students/api";
 import Header from "@/features/Dashboard/Students/components/Header";
@@ -10,7 +11,7 @@ import Main from "@/features/Dashboard/Students/components/Main";
 import Footer from "@/features/Dashboard/Students/components/Footer";
 
 export default function Students() {
-  const [studentList, setStudentList] = useState([]);
+  const { studentList, setStudentList } = useStudentListContext();
 
   const url = `${baseUrl}?limit=5&select=firstName,lastName,email,phone,image,company,website`;
   const options = useMemo(() => ({ method: "GET", cache: "force-cache" }), []);
@@ -21,7 +22,9 @@ export default function Students() {
 
     const rebasedData = rebaseData(data?.users);
     setStudentList(rebasedData);
-  }, [data]);
+  }, [data, setStudentList]);
+
+  if (loading) return;
 
   return (
     <>
