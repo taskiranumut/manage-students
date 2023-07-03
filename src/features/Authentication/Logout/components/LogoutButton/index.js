ce1ed8c71d151f7ab05d5217214628086ecaee8b
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-04 02:58:34 +0300

    feat: redirect to sign-in route when clicked logout button

--- src/features/Authentication/Logout/components/LogoutButton/index.js
+++ src/features/Authentication/Logout/components/LogoutButton/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import styles from "./style.module.css";
 import FromBracket from "@/components/Icons/FromBracket";
+import Link from "next/link";
 
 export default function LogoutButton() {
   return (
-    <button type="button" className={styles.btn}>
-      <span>Logout</span>
-      <div className={styles.iconWrapper}>
-        <span>
-          <FromBracket />
-        </span>
-      </div>
-    </button>
+    <Link href="/sign-in">
+      <button type="button" className={styles.btn}>
+        <span>Logout</span>
+        <div className={styles.iconWrapper}>
+          <span>
+            <FromBracket />
+          </span>
+        </div>
+      </button>
+    </Link>
   );
 }
