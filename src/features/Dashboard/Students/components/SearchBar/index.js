d419c30aa03a0c796772fe8b3f0d78c0b36c5c61
Umut Taşkıran <umut.taskiran7@gmail.com>
2023-07-04 12:16:04 +0300

    feat: add reset button for searchbar

--- src/features/Dashboard/Students/components/SearchBar/index.js
+++ src/features/Dashboard/Students/components/SearchBar/index.js
@@ -3,15 +3,25 @@ import styles from "./styles.module.css";
 import Search from "@/components/Icons/Search";
 import { useGlobalContext } from "@/contexts/StudentList/context";
 import Form from "@/components/Forms/Form";
+import Button from "@/components/Forms/Button";
+import Cross from "@/components/Icons/Cross";
 
 export default function SearchBar() {
   const [form, setForm] = useState({ query: "" });
-  const { setSearchQuery } = useGlobalContext();
+  const { searchQuery, setSearchQuery } = useGlobalContext();
 
   const handleChange = (e) => {
     setForm({
-      [e.target.name]: [e.target.value],
+      [e.target.name]: e.target.value,
     });
+  };
+
+  const handleReset = () => {
+    setForm({ query: "" });
+
+    if (searchQuery) {
+      setSearchQuery("");
+    }
   };
 
   const handleSubmit = (e) => {
@@ -20,20 +30,31 @@ export default function SearchBar() {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <div className={styles.inputWrapper}>
-        <input
-          type="text"
-          name="query"
-          className={styles.input}
-          placeholder="Search..."
-          onChange={handleChange}
-          value={form.query}
-        />
-        <button type="submit" className={styles.iconBtn}>
-          <Search />
-        </button>
-      </div>
-    </Form>
+    <div className={styles.formWrapper}>
+      <Form onSubmit={handleSubmit}>
+        <div className={styles.inputWrapper}>
+          <input
+            type="text"
+            name="query"
+            className={styles.input}
+            placeholder="Search..."
+            onChange={handleChange}
+            value={form.query}
+          />
+          <button type="submit" className={styles.iconBtn}>
+            <Search />
+          </button>
+        </div>
+        <div className={styles.resetBtn}>
+          {form.query && (
+            <Button type="button" btnStyle="icon" onClick={handleReset}>
+              <span>
+                <Cross />
+              </span>
+            </Button>
+          )}
+        </div>
+      </Form>
+    </div>
   );
 }
